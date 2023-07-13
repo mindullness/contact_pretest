c32eaa05716ceae88eeb74dc4e41c914ecf45093
mindullness <toannts2109005@fpt.edu.vn>
2023-07-13 16:45:06 +0700

    git merge r13

--- lib/test_git_protect_branch/toan/main/r1.dart
+++ lib/test_git_protect_branch/toan/main/r1.dart
@@ -1,13 +1,6 @@
 // This is R1
-<<<<<<< HEAD
 // BEFORE THIS IS R12
 
 // THIS IS R13
 
 // THIS IS R12
-=======
-
-// THIS IS R12
-
-// THIS IS R13
->>>>>>> 0f12ecbce9bddd3c8a30778ba6e54072a27b848a
